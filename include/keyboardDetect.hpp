1d497bf25d73f2702bad1fde7390e3dd68f337fc
YoungSusanoo <ksovfox@mail.ru>
2024-07-17 11:09:51 +0500

    Added keyrox tkl detection

---
+++ include/keyboardDetect.hpp
@@ -0,0 +1,8 @@
+#ifndef KEYBOARDDETECT_HPP
+#define KEYBOARDDETECT_HPP
+
+#include <hidapi/hidapi.h>
+
+hid_device* detectKeyboard();
+
+#endif
---
+++ src/keyboardDetect.cpp
@@ -0,0 +1,28 @@
+#include "../include/keyboardDetect.hpp"
+
+#include <stdexcept>
+
+hid_device* detectKeyboard()
+{
+  size_t redSquareVendorId = 0x1A2C;
+  size_t keyroxTklProductId = 0x1511;
+  size_t usagePage = 0xFF00;
+  size_t usage = 2;
+  hid_device_info* infoList = hid_enumerate(redSquareVendorId, keyroxTklProductId);
+  while (infoList->usage_page != usagePage && infoList->usage != usage)
+  {
+    if (!infoList->next)
+    {
+      hid_free_enumeration(infoList);
+      throw std::invalid_argument("keyboardDetect.cpp: !infoList->next");
+    }
+    infoList = infoList->next;
+  }
+  hid_device* keyboardDevice = hid_open_path(infoList->path);
+  hid_free_enumeration(infoList);
+  if (!keyboardDevice)
+  {
+    throw std::invalid_argument("keyboardDetect.cpp: !keyboardDevice");
+  }
+  return keyboardDevice;
+}
